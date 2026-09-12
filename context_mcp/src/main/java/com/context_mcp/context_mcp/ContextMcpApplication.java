69cd30169bcbda608eae683f0596bd2f5e3e333b
Sumit4374 <kusumit437@gmail.com>
2026-09-12 20:19:05 +0530

    first commit

---
+++ context_mcp/src/main/java/com/context_mcp/context_mcp/ContextMcpApplication.java
@@ -0,0 +1,13 @@
+package com.context_mcp.context_mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextMcpApplication.class, args);
+	}
+
+}
---
+++ context_mcp/src/test/java/com/context_mcp/context_mcp/ContextMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.context_mcp.context_mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
